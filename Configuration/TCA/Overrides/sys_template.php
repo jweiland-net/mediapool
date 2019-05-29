254bca87c2a29c2b2f2f52bcbf175197a3de13f9
Stefan Froemken <froemken@gmail.com>
2019-05-29 09:14:35 +0200

    Wrap very long lines in Overrides

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,5 +3,13 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mediapool', 'Configuration/TypoScript/Mediapool', 'Mediapool Video/Playlist');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('mediapool', 'Configuration/TypoScript/Gallery', 'Mediapool Gallery');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'mediapool',
+    'Configuration/TypoScript/Mediapool',
+    'Mediapool Video/Playlist'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'mediapool',
+    'Configuration/TypoScript/Gallery',
+    'Mediapool Gallery'
+);
