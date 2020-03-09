a2d1f90b651bff0db9ae31b83a06a5d1d803a339
Pascal Rinker <info@kronova.net>
2020-03-09 13:34:08 +0100

    [TASK] Move registerPlugin calls from ext_tables to tt_content overrides file
    Fixes issue #4

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,22 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+// Register main plugin
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'mediapool',
+    'Mediapool',
+    'LLL:EXT:mediapool/Resources/Private/Language/locallang_db.xlf:plugin.mediapool.title',
+    'EXT:mediapool/Resources/Public/Icons/video_play.svg'
+);
+
+// Register gallery plugin
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'mediapool',
+    'Gallery',
+    'LLL:EXT:mediapool/Resources/Private/Language/locallang_db.xlf:plugin.gallery.title',
+    'EXT:mediapool/Resources/Public/Icons/gallery.svg'
+);
+
 // Add flexform for Mediapool plugin
 $pluginSignature = 'mediapool_mediapool';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'select_key,pages,recursive';
--- ext_tables.php
+++ ext_tables.php
@@ -1,19 +1,3 @@
 <?php
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mediapool_domain_model_video');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_mediapool_domain_model_playlist');
-
-// Register main plugin
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'mediapool',
-    'Mediapool',
-    'LLL:EXT:mediapool/Resources/Private/Language/locallang_db.xlf:plugin.mediapool.title',
-    'EXT:mediapool/Resources/Public/Icons/video_play.svg'
-);
-
-// Register gallery plugin
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'mediapool',
-    'Gallery',
-    'LLL:EXT:mediapool/Resources/Private/Language/locallang_db.xlf:plugin.gallery.title',
-    'EXT:mediapool/Resources/Public/Icons/gallery.svg'
-);
