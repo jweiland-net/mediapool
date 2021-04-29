75646b7c9d57d7360371c083f6d5da412630ac6d
Stefan Froemken <froemken@gmail.com>
2021-04-29 14:48:31 +0200

    Update PHP Doc Header

--- Classes/Form/Element/VideoTextElement.php
+++ Classes/Form/Element/VideoTextElement.php
@@ -1,18 +1,15 @@
 <?php
-namespace JWeiland\Mediapool\Form\Element;
+
+declare(strict_types=1);
 
 /*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+ * This file is part of the package jweiland/mediapool.
  *
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * LICENSE file that was distributed with this source code.
  */
+
+namespace JWeiland\Mediapool\Form\Element;
 
 use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
