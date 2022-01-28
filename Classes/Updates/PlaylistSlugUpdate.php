d993eefa7486e69252cd80aad065a3f6f73485f1
Pascal Rinker <info@kronova.net>
2022-01-28 13:37:37 +0100

    [TASK] Fix copyright notice

--- Classes/Updates/PlaylistSlugUpdate.php
+++ Classes/Updates/PlaylistSlugUpdate.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/5160001-drs-portal.
+ * This file is part of the package jweiland/mediapool.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
