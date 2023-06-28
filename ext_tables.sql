ddf6647c9880e45d159a54a7fb70393bccf8d243
Stefan Froemken <sfroemken@jweiland.net>
2023-06-28 16:02:01 +0200

    Add missing slug colum for playlist

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,14 +4,14 @@
 CREATE TABLE tx_mediapool_domain_model_video
 (
 	title           varchar(100) DEFAULT '' NOT NULL,
+	slug            varchar(2048),
 	description     text         DEFAULT '' NOT NULL,
 	upload_date     int(11) unsigned DEFAULT '0' NOT NULL,
 	link            varchar(100) DEFAULT '' NOT NULL,
 	player_html     text         DEFAULT '' NOT NULL,
 	video_id        varchar(100) DEFAULT '' NOT NULL,
 	thumbnail       varchar(100) DEFAULT '' NOT NULL,
-	thumbnail_large varchar(100) DEFAULT '' NOT NULL,
-	slug            varchar(2048)
+	thumbnail_large varchar(100) DEFAULT '' NOT NULL
 );
 
 #
@@ -19,10 +19,11 @@ CREATE TABLE tx_mediapool_domain_model_video
 #
 CREATE TABLE tx_mediapool_domain_model_playlist
 (
-	title         varchar(100) DEFAULT '' NOT NULL,
-	link          varchar(100) DEFAULT '' NOT NULL,
-	playlist_id   varchar(100) DEFAULT '' NOT NULL,
-	videos        int(11) unsigned DEFAULT '0' NOT NULL,
-	categories    int(11) DEFAULT '0' NOT NULL,
-	thumbnail     varchar(100) DEFAULT '' NOT NULL
+	title       varchar(100) DEFAULT '' NOT NULL,
+	slug        varchar(2048),
+	link        varchar(100) DEFAULT '' NOT NULL,
+	playlist_id varchar(100) DEFAULT '' NOT NULL,
+	videos      int(11) unsigned DEFAULT '0' NOT NULL,
+	categories  int(11) DEFAULT '0' NOT NULL,
+	thumbnail   varchar(100) DEFAULT '' NOT NULL
 );
