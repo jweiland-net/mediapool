38748af8de278ec7c38c5d1029152c5834ba7a2f
Stefan Froemken <sfroemken@jweiland.net>
2023-06-28 15:32:32 +0200

    Add missing categories column

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,91 +1,28 @@
 #
 # Table structure for table 'tx_mediapool_domain_model_video'
 #
-CREATE TABLE tx_mediapool_domain_model_video (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	title varchar(100) DEFAULT '' NOT NULL,
-	description text DEFAULT '' NOT NULL,
-	upload_date int(11) unsigned DEFAULT '0' NOT NULL,
-	link varchar(100) DEFAULT '' NOT NULL,
-	player_html text DEFAULT '' NOT NULL,
-	video_id varchar(100) DEFAULT '' NOT NULL,
-	thumbnail varchar(100) DEFAULT '' NOT NULL,
+CREATE TABLE tx_mediapool_domain_model_video
+(
+	title           varchar(100) DEFAULT '' NOT NULL,
+	description     text         DEFAULT '' NOT NULL,
+	upload_date     int(11) unsigned DEFAULT '0' NOT NULL,
+	link            varchar(100) DEFAULT '' NOT NULL,
+	player_html     text         DEFAULT '' NOT NULL,
+	video_id        varchar(100) DEFAULT '' NOT NULL,
+	thumbnail       varchar(100) DEFAULT '' NOT NULL,
 	thumbnail_large varchar(100) DEFAULT '' NOT NULL,
-	slug varchar(2048),
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+	slug            varchar(2048)
 );
 
 #
 # Table structure for table 'tx_mediapool_domain_model_playlist'
 #
-CREATE TABLE tx_mediapool_domain_model_playlist (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	title varchar(100) DEFAULT '' NOT NULL,
-	link varchar(100) DEFAULT '' NOT NULL,
-	playlist_id varchar(100) DEFAULT '' NOT NULL,
-	videos int(11) unsigned DEFAULT '0' NOT NULL,
-	thumbnail varchar(100) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-	slug varchar(2048),
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
-);
-
-#
-# Table structure for table 'tx_mediapool_playlist_video_mm'
-#
-CREATE TABLE tx_mediapool_playlist_video_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
+CREATE TABLE tx_mediapool_domain_model_playlist
+(
+	title         varchar(100) DEFAULT '' NOT NULL,
+	link          varchar(100) DEFAULT '' NOT NULL,
+	playlist_id   varchar(100) DEFAULT '' NOT NULL,
+	videos        int(11) unsigned DEFAULT '0' NOT NULL,
+	categories    int(11) DEFAULT '0' NOT NULL,
+	thumbnail     varchar(100) DEFAULT '' NOT NULL
 );
