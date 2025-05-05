3d282d6864a51746bb2a33355c27f84f4676b0af
Stefan Froemken <sfroemken@jweiland.net>
2025-05-05 10:47:01 +0200

    Update typo3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * The source of icons used in this extension is
  * https://www.google.com/design/icons/ (License: https://github.com/google/material-design-icons/blob/master/LICENSE)
  * https://icomoon.io Pack IcoMoon - Free (License: https://creativecommons.org/licenses/by/4.0/)
  */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Mediapool',
     'description' => 'Embed your favorite YouTube Videos and Playlists. Import description, title and more by just pasting the YouTube link.',
@@ -15,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.36-11.5.23',
+            'typo3' => '11.5.23-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
