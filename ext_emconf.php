3e885dad10811612f8753ef4f9ad252a36791ff5
Stefan Froemken <sfroemken@jweiland.net>
2025-05-19 11:35:18 +0200

    Add TYPO3 13 compatibility. Remove TYPO3 11/12 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.40-12.4.99',
+            'typo3' => '13.4.3-13.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
