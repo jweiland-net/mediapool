8fc568d8a7a5f003f5d62795bfcc8594a83b3684
Stefan Froemken <sfroemken@jweiland.net>
2025-07-28 14:52:43 +0200

    Update doc header in UpgradeWizard

--- Classes/Upgrade/AddUidAsAutoIncrementToMediapoolReferenceTableUpgrade.php
+++ Classes/Upgrade/AddUidAsAutoIncrementToMediapoolReferenceTableUpgrade.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/jw5160001drsportal.
+ * This file is part of the package jweiland/mediapool.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
